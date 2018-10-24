60d6adf4b50317c2bd2bb7e3a40862820d8ed083
saalome <saijp8@gmail.com>
2018-10-24 15:38:56 +0800

    一些solo learn上的题目

--- git_1.py
+++ git_1.py
@@ -1,5 +1,31 @@
-a = 'hello world'
-print(a)
-print('JFC says:' + a)
-print('good')
-print('yes')
+# nums=[1,2,3]
+# print(nums+[4,5,6])
+# print(nums*3)
+#
+# words = ["spam", "egg", "spam", "sausage"]
+# print("spam" in words)
+# print("egg" in words)
+# print("tomato" in words)
+
+# nums = [10, 9, 8, 7, 6, 5]
+# nums[0] = nums[1] - 5
+# if 4 in nums:
+#   print(nums[3])
+# else:
+#   print(nums[4])
+
+# nums = [1, 2, 3]
+# print(not 4 in nums)
+# print(4 not in nums)
+# print(not 3 in nums)
+# print(3 not in nums)
+
+# letters = ['a', 'b', 'z']
+# if"z"in letters:
+#     print("Yes")
+
+
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+print(nums[3])
