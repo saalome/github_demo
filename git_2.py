1da8d1294c89b8d275c7c4853af1720a7533d0ec
saalome <saijp8@gmail.com>
2018-10-25 16:26:32 +0800

    Python 练习1

--- git_2.py
+++ git_2.py
@@ -1,8 +1,50 @@
-# 使用while循环输入 1 2 3 4 5 6     8 9 10
-while True:
-    number = int(input('请输入一个数字:'))
-    if number > 10:
-        break
-    elif number == 7:
-        continue
-    print(number)
+# # 使用while循环输入 1 2 3 4 5 6     8 9 10
+# while True:
+#     number = int(input('请输入一个数字:'))
+#     if number > 10:
+#         break
+#     elif number == 7:
+#         continue
+#     print(number)
+
+
+# 求1-100的所有数的和
+'''
+num = list(range(1,101))
+sum = 0
+for i in  num:
+    sum = sum +i
+    print(sum)
+'''
+
+
+
+# 3、输出 1-100 内的所有奇数
+
+# for i in range(1,101,2):
+#     print(i)
+
+
+# a = 1
+# while True:
+#     if a%2 == 1:
+#         print(a)
+#     if a == 100:
+#         break
+#     a = a + 1
+
+
+# #4、输出 1-100 内的所有偶数
+# for i in range(2,101,2):
+#     print(i)
+
+# 求1 - 2 + 3 - 4 + 5...
+# 99
+# 的所有数的和
+a = list(range(1,101,2))
+b = list(range(2,101,2))
+for i in c:
+    print(i)
+
+
+
